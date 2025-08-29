13d5a44a7b1f85c958b33a34f5adb47cb17808ec
Grm-gh <gyanurajmaniar@gmail.com>
2025-08-29 22:11:22 +0530

    added alumni and their data

---
+++ frontend/src/data/profile.js
@@ -0,0 +1,235 @@
+const  team = [
+  {
+    "name": "Ravish Jain",
+    "role": "president",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/C4D03AQGNs8XPNXSgpw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1658255428227?e=2147483647&v=beta&t=UAdsOJXcl2dH3kcxu9hiwEW9OdC8Y-lXo9aVcrgvgIs",
+    "experience": [
+      {
+        "role": "business analyst",
+        "company": "porter"
+      },
+      {
+        "role": "business analyst",
+        "company": "jupiter"
+      },
+      {
+        "role": "Data Engineer",
+        "company": "Axis bank"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/ravish-jain-68791b172"
+  },
+  {
+    "name": "vathsalya Reddy",
+    "role": "Vice-president",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D5603AQEde9mHJJxvmA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1686932857903?e=2147483647&v=beta&t=6KVWTs-2MuXPR9Ycc_-vsizBLkA3H6z3rcqd9nWbYtg",
+    "experience": [
+      {
+        "role": "Management consultant analyst",
+        "company": "Accenture stratergy & consulting"
+      },
+      {
+        "role": "Analyst",
+        "company": "deloitte consulting"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/vathsalya-reddy-a3869916b/"
+  },
+  {
+    "name": "Vishesh Sajgotra",
+    "role": "Vice-president",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/C5103AQFEcdcTnt0XuQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1566668308409?e=2147483647&v=beta&t=AJJtqJHaAWvqbn4qdpAbR-_e3xFjaqkP2qjoH2AXgNE",
+    "experience": [
+      {
+        "role": "Senior Trading Analyst",
+        "company": "GIM"
+      },
+      {
+        "role": "Market Analyst",
+        "company": "Futures First"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/vishesh-sajgotra-4b8350175"
+  },
+  {
+    "name": "Meet Joshi",
+    "role": "",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/C5603AQEgKJIe7vJkkA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1609826325380?e=2147483647&v=beta&t=v-c6F09GnxGQUvguEy_AcevAj9z30PFw5Z0XGzYOs8w",
+    "experience": [
+      {
+        "role": "R&D Engineer",
+        "company": "Orient electric Ltd"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/vivek-subhash-shinde"
+  },
+  {
+    "name": "Aryan",
+    "role": "President",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D4E35AQFiktktnkqt7g/profile-framedphoto-shrink_400_400/B4EZc3H2D7HcAg-/0/1748976493053?e=1756926000&v=beta&t=TqBj1p2c3nu1bwqKClFXk7L9UtCeexEvdCuD9_QbKec",
+    "experience": [
+      {
+        "role": "AI Developer",
+        "company": "TCS"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/aryan-bitm/"
+  },
+  {
+    "name": "Ansh Trivedi",
+    "role": "Vice-president",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D4D03AQF8wVC6O12Mkw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726090405700?e=1759363200&v=beta&t=cgCkM8gehMPSIqqHx74IoNRJxKTCbLvnbPLXtynKbhc",
+    "experience": [
+      {
+        "role": "Associate",
+        "company": "PwC"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/ansh-trivedi-4278b0193/"
+  },
+  {
+    "name": "Anoushka Singh",
+    "role": "Vice-president",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D4D03AQHrmT23K1kRmA/profile-displayphoto-shrink_400_400/B4DZTU8wnDG8Ag-/0/1738739486667?e=1759363200&v=beta&t=DEVWmDdhJ5cghUhyZOuxHaGVxh5AqSZ-SnIlAj-gbUw",
+    "experience": [
+      {
+        "role": "Intern",
+        "company": "KPMG"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/anoushka-singh-3a0008232/"
+  },
+  {
+    "name": "Ankur Pandey",
+    "role": "Director",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D5603AQEwkzeE3sKrMA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1715875136629?e=1759363200&v=beta&t=fDtMuJOtlWMQif02YEUKKbvXO7WHkJRGCu-LpJQxECg",
+    "experience": [
+      {
+        "role": "SDE",
+        "company": "Labra.io"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/ankur-pandey07/"
+  },
+  {
+    "name": "Arya Shreshth",
+    "role": "General Secretary",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D4D03AQG86vQqoLFfMQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1703164920350?e=1759363200&v=beta&t=eBOdtjHRg7KAAk-xKsnMQGvOOfsABTFPdsJApm-nPrs",
+    "experience": [
+      {
+        "role": "SDE",
+        "company": "Walmart"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/arya-shreshth/"
+  },
+  {
+    "name": "Kritika Jain",
+    "role": "Joint Secretary",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D5603AQEy4XsgOwYH9w/profile-displayphoto-crop_800_800/B56Zi3P.IkHAAI-/0/1755421075144?e=1759363200&v=beta&t=Q8rpbx97BzJGGSljIiLnjdlsj3OYgUHE7lAYC4N6uJ0",
+    "experience": [
+      {
+        "role": "Intern",
+        "company": "Fastenal"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/kritika-jain-kj/"
+  },
+  {
+    "name": "Amitesh Kumar Sinha",
+    "role": "Joint Secretary",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D5635AQFJb6gGAS3jpA/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1705983217686?e=1756926000&v=beta&t=EcCFgprjRcsZ2h4ennG-d5uig2-1iEm2MoENfum-jPA",
+    "experience": [
+      {
+        "role": "NA",
+        "company": "NA"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/amitesh-kumar-sinha-gt69/"
+  },
+  {
+    "name": "Mayank Giri",
+    "role": "Treasurer",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D5603AQEF8tK4uiPlYQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1715095641879?e=1759363200&v=beta&t=tXxo6ePVTbj2jjQRSQLMps-VdIBZl4ebhsLpjLE4Szc",
+    "experience": [
+      {
+        "role": "Intern",
+        "company": "IIT ISM"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/mayank-giri-66b24321b/"
+  },
+  {
+    "name": "Akansha khedia",
+    "role": "president ",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D4D03AQEHcoKRY_dekQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1666476208086?e=1759363200&v=beta&t=wu_sAouRgad86RTsUP47ofPGeKi_GIleOmQJs93FgY0",
+    "experience": [
+      {
+        "role": "software engineer ",
+        "company": "microsoft"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/akansha-khedia-0a82b5167/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": "prabal Gupta",
+    "role": "vice president ",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D4D03AQGp5_2URZuxfw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1672383963628?e=1759363200&v=beta&t=gArqKYiBNchNj9_1_9-rBZf5lCtf5xGXfXMTMVzSH-w",
+    "experience": [
+      {
+        "role": "Product Designer ",
+        "company": "SplashLearn"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/prabalgupta99/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": "Ansh Trivedi",
+    "role": "vice president ",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D4D03AQF8wVC6O12Mkw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726090405700?e=1759363200&v=beta&t=cgCkM8gehMPSIqqHx74IoNRJxKTCbLvnbPLXtynKbhc",
+    "experience": [
+      {
+        "role": "Associate ",
+        "company": "PWC"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/ansh-trivedi-4278b0193/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": "Ankur Pandey",
+    "role": "Joint Secretary",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D5603AQEwkzeE3sKrMA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1715875136629?e=1759363200&v=beta&t=fDtMuJOtlWMQif02YEUKKbvXO7WHkJRGCu-LpJQxECg",
+    "experience": [
+      {
+        "role": "SDE  ",
+        "company": "Labra.io"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/ankur-pandey07/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": " Naman jain",
+    "role": "Joint Secretary",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D4D03AQH5ovT2K8rdDA/profile-displayphoto-shrink_400_400/B4DZUKr014HwAg-/0/1739641017546?e=1759363200&v=beta&t=p_bKuyCMTi8GnaEAe1Zqhtqq5y47ixfMNSJ4lUmI3Cg",
+    "experience": [
+      {
+        "role": "System Engineer",
+        "company": "TCS"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/namanjain0210/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    "name": "Kajal Kiran",
+    "role": "Joint Treasurer",
+    "profile_photo": "https://media.licdn.com/dms/image/v2/D5603AQEyzGyYkBKr9A/profile-displayphoto-shrink_400_400/B56ZTZ._uCHEAg-/0/1738823958796?e=1759363200&v=beta&t=drAn5nNBgDRdQcgm_bwSaM5IBR-AMDE_VbhK09CA8T4",
+    "experience": [
+      {
+        "role": "SDE",
+        "company": "Fastenal"
+      }
+    ],
+    "linkedin": "https://www.linkedin.com/in/kajal-kiran-4a82b5217/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  }
+];
+export default team;
